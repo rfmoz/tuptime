48a53ff5dbce19e25877a75a0667a93b6743a90e
Ricardo F <rfraile@rfraile.eu>
2015-09-16 14:29:09 +0200

    Cleaning code, clear output, minnor code improvements

---
+++ scripts/db-migrate.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+# This script update the tuptime database format from versions previous 3.0.00
+
+# Change the db origin:
+#      uptime real, btime integer, shutdown integer
+# to:
+#      uptime real, btime integer, pwoff_state integer, downtime real, offbtime integer
+
+SOURCE_DB='/var/lib/tuptime/tuptime.db'
+
+# Test file permissions
+if [ -w "${SOURCE_DB}" ]; then
+   echo "Migrating tuptime database format"
+else
+   echo "Please, execute this script with a privileged user that can write in: ${SOURCE_DB}"
+   exit 1
+fi
+
+# Test sqlite3 command
+sqlite3 -version > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Please, install "sqlite3" command for manage sqlite v3 databases."
+        exit 2
+fi
+
+TMP_DB=`mktemp`  # For temporary process db
+
+cp ${SOURCE_DB} ${TMP_DB}
+
+# Check db format
+sqlite3 ${TMP_DB} "PRAGMA table_info(tuptime);" | grep -E 'pwoff_state|downtime|offbtime' > /dev/null 
+if [ $? -eq 0 ]; then
+	echo "Database is already in new format"
+        exit 3
+fi
+
+# Change shutdown column to pwoff_state
+sqlite3 ${TMP_DB} "ALTER TABLE tuptime RENAME TO tuptime_old;"
+
+sqlite3 ${TMP_DB} "CREATE TABLE tuptime (uptime REAL, btime INT, pwoff_state INT, downtime REAL, offbtime INT);"
+
+sqlite3 ${TMP_DB} "INSERT INTO tuptime(uptime, btime, pwoff_state) SELECT uptime, btime, shutdown FROM tuptime_old;"
+
+sqlite3 ${TMP_DB} "DROP TABLE tuptime_old;"
+
+# Adding values for new columns downtime and offbtime
+ROWS=`sqlite3 ${TMP_DB} "select max(oid) from tuptime;"`
+
+for I in $(seq 1 ${ROWS}); do
+	UPTIME=`sqlite3 ${TMP_DB} "SELECT uptime from tuptime where oid = ${I};"`
+	BTIME=`sqlite3 ${TMP_DB} "SELECT btime from tuptime where oid = ${I};"`
+        Z=$((I+1))
+	NEXT_BTIME=`sqlite3 ${TMP_DB} "SELECT btime from tuptime where oid = ${Z};"`
+
+	OFFBTIME=$(echo ${UPTIME} + ${BTIME} | bc)
+	DOWNBTIME=$(echo ${NEXT_BTIME} - ${OFFBTIME} | bc)
+
+	sqlite3 ${TMP_DB} "UPDATE tuptime SET downtime = ${DOWNBTIME}, offbtime = ${OFFBTIME} where oid = ${I}"
+	
+done
+
+# Clear last row shutdown values
+sqlite3 ${TMP_DB} "UPDATE tuptime SET downtime = '-1', offbtime = '-1' where oid = ${I}" 
+
+# Backup old db and restore the new
+mv ${SOURCE_DB} ${SOURCE_DB}.back
+mv ${TMP_DB} ${SOURCE_DB}
+chown 644 ${SOURCE_DB}
+
+rm -f ${TMP_DB}
+
+echo "Backup file in: ${SOURCE_DB}.back"
+echo "Process completed OK"
+
