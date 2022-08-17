d4bf7edd0e5b53049fefc5d1e56ac5e5b045fcd6
Ricardo F <rfraile@rfraile.eu>
2022-08-17 21:15:07 +0200

    Fix cron.d execution on Debian

--- debian/tuptime.cron.d
+++ debian/tuptime.cron.d
@@ -2,9 +2,7 @@
 
 # NOTE: Decrease the execution time for increase accuracity.
 
-
 # Skip in favour of systemd timer
-[ ! -d /run/systemd/system ] || exit 0
 
-*/5 * * * *   _tuptime    if [ -x /usr/bin/tuptime ]; then /usr/bin/tuptime -x > /dev/null; fi
+*/5 * * * *   _tuptime    if [ ! -d /run/systemd/system ] && [ -x /usr/bin/tuptime ]; then /usr/bin/tuptime -x > /dev/null; fi
 
